b35186fe956a353df48668bc7225ff22a36379ab
lanser <lanser@111.com>
2017-11-19 14:35:29 +0800

    2017-11-9

---
+++ shop/js/result.js
@@ -0,0 +1,64 @@
+webpackJsonp([5],{
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(138);
+
+
+/***/ }),
+
+/***/ 138:
+/***/ (function(module, exports, __webpack_require__) {
+
+	/*
+	* @Author: Rosen
+	* @Date:   2017-05-19 21:52:46
+	* @Last Modified by:   Rosen
+	* @Last Modified time: 2017-05-19 23:01:25
+	*/
+
+	'use strict';
+	__webpack_require__(139);
+	__webpack_require__(141);
+	var _mm = __webpack_require__(10);
+
+	$(function(){
+	    var type        = _mm.getUrlParam('type') || 'default',
+	        $element    = $('.' + type + '-success');
+	    // 显示对应的提示元素
+	    $element.show();
+	})
+
+/***/ }),
+
+/***/ 139:
+/***/ (function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 141:
+/***/ (function(module, exports, __webpack_require__) {
+
+	/*
+	* @Author: Rosen
+	* @Date:   2017-05-17 11:26:25
+	* @Last Modified by:   Rosen
+	* @Last Modified time: 2017-05-17 11:26:46
+	*/
+
+	'use strict';
+	__webpack_require__(142);
+
+/***/ }),
+
+/***/ 142:
+/***/ (function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ })
+
+});
