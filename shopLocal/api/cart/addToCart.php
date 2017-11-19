4d9dce911dc3186455a6a1a4b4e3de47c47b5756
lanser <lanser@111.com>
2017-11-19 13:49:34 +0800

    删除

--- shopLocal/api/cart/addToCart.php
+++
@@ -1,8 +0,0 @@
-<?php
-//防止返回到前端时数据错乱
-header('content-type:text/html; charset= utf-8');
-//echo$_GET['location'];
-$parameters = $_SERVER['QUERY_STRING'];
-$url = 'http://happymmall.com/cart/add.do?'.$parameters;
-echo file_get_contents($url);
-?>
